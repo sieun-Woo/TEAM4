f1f9e53a81ba25aaa2006b8a227fc86c02215dcb
byeongdoohan <quden04@gmail.com>
2023-01-17 20:10:48 +0900

    admin entity 추가

--- shoppingmall/src/main/java/com/sparta/shoppingmall/admin/entity/Admin.java
+++ shoppingmall/src/main/java/com/sparta/shoppingmall/admin/entity/Admin.java
@@ -1,5 +1,21 @@
 package com.sparta.shoppingmall.admin.entity;
 
-public class Admin {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Admin extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "admin_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private
 
 }
