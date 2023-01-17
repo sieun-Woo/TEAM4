84b42ccae053e3be99575e79b3a5cf3b56409d1d
byeongdoohan <quden04@gmail.com>
2023-01-17 20:10:26 +0900

    admincontroller 추가

--- shoppingmall/src/main/java/com/sparta/shoppingmall/admin/controller/AdminController.java
+++ shoppingmall/src/main/java/com/sparta/shoppingmall/admin/controller/AdminController.java
@@ -22,6 +22,7 @@ public class AdminController {
     //판매자 등록 승인
     @PutMapping("/admin/seller-registration/{authId}")
     public ResponseEntity<String> permitSellerRegister(@RequestBody SellerRegistrationDto sellerRegistrationDto){
+        adminServiceImpl.permitSellerRegister();
         return new ResponseEntity<>("판매자 등록이 승인되었습니다.", HttpStatus.OK);
     }
 
