abdb99a373f71a98397923f86862acfe4dadd07c
byeongdoohan <quden04@gmail.com>
2023-01-19 16:16:42 +0900

    SellerRegistrationDto.java 삭제

--- shoppingmall/src/main/java/com/sparta/shoppingmall/controller/AdminController.java
+++ shoppingmall/src/main/java/com/sparta/shoppingmall/controller/AdminController.java
@@ -2,16 +2,12 @@ package com.sparta.shoppingmall.controller;
 
 import com.sparta.shoppingmall.dto.CustomerProfileResponseDto;
 import com.sparta.shoppingmall.dto.SellerProfileResponseDto;
-import com.sparta.shoppingmall.dto.SellerRegistrationDto;
-import com.sparta.shoppingmall.service.AdminService;
 import com.sparta.shoppingmall.service.AdminServiceImpl;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
