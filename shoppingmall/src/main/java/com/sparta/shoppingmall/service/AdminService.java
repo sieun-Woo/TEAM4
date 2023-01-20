65fc97ac3a52d0e5c15264f9470a04c78db3a715
byeongdoohan <quden04@gmail.com>
2023-01-20 16:01:02 +0900

    admin 서비스 구현

--- shoppingmall/src/main/java/com/sparta/shoppingmall/service/AdminService.java
+++ shoppingmall/src/main/java/com/sparta/shoppingmall/service/AdminService.java
@@ -1,18 +1,23 @@
 package com.sparta.shoppingmall.service;
 
-import com.sparta.shoppingmall.dto.CustomerResponseDto;
+import com.sparta.shoppingmall.dto.CustomerProfileResponseDto;
+import com.sparta.shoppingmall.dto.RegistrationRequestDto;
+import com.sparta.shoppingmall.dto.RegistrationResponseDto;
+import com.sparta.shoppingmall.entity.User;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface AdminService {
-    List<CustomerResponseDto> getCustomerList(); //고객 목록 조회
+    List<CustomerProfileResponseDto> getCustomerList(); //고객 목록 조회
 
-    List<SellerRegistrationDto> getSellerRegistrationList(); //판매자 등록 요청 조회
+    List<RegistrationResponseDto> getSellerRegistrationList(); //판매자 등록 요청 조회
 
-    void permitSellerRegister(SellerRegistrationDto sellerRegistrationDto);
     //판매자 권한 허가
 
-    List<SellerProfileResponseDto> getSellerList(); //판매자 목록 조회
+    void permitSellerRegister(RegistrationRequestDto registrationRequestDto, Long id);
 
-    void deleteSellerRegistration(Long authId); //판매자 권한 삭제
+    List<RegistrationResponseDto> getSellerList(); //판매자 목록 조회
+
+    void deleteSellerRegistration(Long id); //판매자 권한 삭제
 }
