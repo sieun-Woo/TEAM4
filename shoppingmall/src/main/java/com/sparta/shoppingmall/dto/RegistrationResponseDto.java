8ab5d5c309a30c5b4d7548275a4f957bd8d90203
byeongdoohan <quden04@gmail.com>
2023-01-20 15:59:45 +0900

    registration -> RegistrationResponseDto 메소드 추가

--- shoppingmall/src/main/java/com/sparta/shoppingmall/dto/RegistrationResponseDto.java
+++ shoppingmall/src/main/java/com/sparta/shoppingmall/dto/RegistrationResponseDto.java
@@ -1,11 +1,31 @@
 package com.sparta.shoppingmall.dto;
 
+import com.sparta.shoppingmall.entity.Registration;
+import com.sparta.shoppingmall.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class RegistrationResponseDto {
+
     private Long id;
-    private
+    private String nickName;
+    private String image;
+    private String intro;
+
+    public RegistrationResponseDto(Registration registration) {
+        this.id = id;
+        this.nickName = nickName;
+        this.image = image;
+        this.intro = intro;
+    }
+
+    public RegistrationResponseDto(User user) {
+        this.id = user.getId();
+    }
+
+    public static RegistrationResponseDto add(Registration registration){
+        return new RegistrationResponseDto(registration);
+    }
 }
